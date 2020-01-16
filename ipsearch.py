e24b3768277ada1316416b2e9fc581a6b16389f8
wonkyungup <34180230+wonkyungup@users.noreply.github.com>
2020-01-16 17:18:20 +0900

    Update ipsearch.py

--- ipsearch.py
+++ ipsearch.py
@@ -23,12 +23,6 @@ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
-
-  Last edited by:  $Author$
-              on:  $DateTime$
-        Revision:  $Revision$
-              Id:  $Id$
-          Author:  Chris Wolf
 """
 from ipwhois import IPWhois
 from tkinter import *
